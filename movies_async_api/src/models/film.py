9529f15fd8e0c4b414b3eacc39dfb294ca3d2031
KirillDomitin <domitin@gmail.com>
2025-04-29 00:38:51 +0300

    starting movies_asinc_api service, add redis to docker-compose.yml

---
+++ movies_async_api/src/models/film.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+# Используем pydantic для упрощения работы при перегонке данных из json в объекты
+from pydantic import BaseModel
+
+
+class Film(BaseModel):
+    id: str
+    title: str
+    description: str
